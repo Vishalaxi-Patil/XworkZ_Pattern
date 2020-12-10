d6e05ca8d728564ebcacef843ff287cd29102de9
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-12-10 07:29:39 +0530

    Add files via upload

---
+++ Pattern/PatternProgram/src/com/xworkz/pattern/SquareOne.java
@@ -0,0 +1,16 @@
+package com.xworkz.pattern;
+
+public class SquareOne {
+	public static void main(String[] args) {
+	for(int i=1;i<=4;i++)
+	{
+		for(int j=4;j>=i;j--)
+		{
+			System.out.print("*");
+			
+		}
+		System.out.println();
+	}
+
+}
+}
